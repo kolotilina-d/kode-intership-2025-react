2187439c0e778c8e3fa27c7e98465a33d9f82f16
Darya Kolotilina <kolotilina.d@gmail.com>
2025-03-15 16:06:59 +0200

    'config changed'

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/kode-intership-2025-react/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  base: '/kode-intership-2025-react/',
 });
 
 
